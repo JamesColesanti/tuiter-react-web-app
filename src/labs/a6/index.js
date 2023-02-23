79dc3590b0e7f11ee2b854114714bcdc0aa20af1
JamesColesanti <79491731+JamesColesanti@users.noreply.github.com>
2023-02-23 00:13:51 -0500

    Started lab

---
+++ src/labs/a6/index.js
@@ -0,0 +1,14 @@
+import Classes from "./classes";
+
+function Assignment6() {
+    return(
+        <div>
+            <h1>Assignment 6</h1>
+            <Classes/>
+        </div>
+    )
+}
+
+export default Assignment6;
+
+    
